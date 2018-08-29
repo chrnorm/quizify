c487817c604bf6ce3d23878cb96bd4d2e6f71c7d
Chris Norman <chris@humm.tech>
2018-08-28 21:09:16 -0700

    server: send smaller album artwork images

--- server/routes.js
+++ server/routes.js
@@ -106,7 +106,7 @@ router.get('/question', (req, res) => {
         name: obj.track.name,
         artists: obj.track.artists.map(artist => artist.name),
         album: obj.track.album.name,
-        artwork: obj.track.album.images[0].url,
+        artwork: obj.track.album.images[1].url, // 300x300 image
         id: obj.track.id
     });
 
