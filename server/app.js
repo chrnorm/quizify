5fbcddd7bdba5161ef98e0ed7d2c5e404a4c92c0
Chris Norman <chris@humm.tech>
2018-08-30 20:26:07 -0700

    server: add redis session store

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const cors = require('cors');
 const logger = require('morgan');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const routes = require('./routes');
 
 const port = process.env.PORT || 8888;
@@ -15,7 +16,15 @@ const app = express();
 
 app.set('port', port);
 app.use(logger('dev'))
-    .use(session({ secret: 'quizify', cookie: { maxAge: 600000 } })) //TODO: implement prod-ready session store
+    .use(
+        session({
+            store: new RedisStore({ host: 'redis', port: 6379 }),
+            secret: 'quizify',
+            cookie: { maxAge: 600000 },
+            resave: false,
+            saveUninitialized: false
+        })
+    ) //TODO: implement prod-ready session store
     .use(cookieParser())
     .use(cors({ origin: 'http://localhost:3000', credentials: true }))
     .use(bodyParser.json())
