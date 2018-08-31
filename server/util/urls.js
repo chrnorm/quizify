77741c9965c1ef1dd778b93c28352bec9e8541dc
Chris Norman <chris@humm.tech>
2018-09-01 09:37:37 +1000

    server: fix module.exports for URLs, add start-prod command which points to the deployed client app

--- server/util/urls.js
+++ server/util/urls.js
@@ -1,7 +1,7 @@
 let CLIENT_URL;
 
-process.env.NODE_ENV === 'dev'
+process.env.NODE_ENV !== 'production'
     ? (CLIENT_URL = 'http://localhost:3000')
     : (CLIENT_URL = 'http://quizify.app'); // digitalocean droplet public ip
 
-export default CLIENT_URL;
+module.exports = CLIENT_URL;
