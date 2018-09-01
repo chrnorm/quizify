776de56da4f5bdbad0e4d9b78e06d7266a146225
Chris Norman <chris@humm.tech>
2018-09-01 15:02:39 +0800

    client: change prod url to heroku

--- quizifyclient/src/util/apiUrl.js
+++ quizifyclient/src/util/apiUrl.js
@@ -2,6 +2,6 @@ let API_URL;
 
 process.env.REACT_APP_STAGE === 'dev'
     ? (API_URL = 'http://localhost:8888')
-    : (API_URL = 'http://178.128.21.154:8888'); // digitalocean droplet public ip
+    : (API_URL = 'https://quizify-server.herokuapp.com');
 
 export default API_URL;
