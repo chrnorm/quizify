204859b1b9dc467ad116e17f6bb21b5ead954c09
Chris Norman <chris@humm.tech>
2018-12-15 20:34:38 +0800

    remove unused files

--- src/util/apiUrl.js
+++
@@ -1,7 +0,0 @@
-let API_URL;
-
-process.env.REACT_APP_STAGE === 'dev'
-    ? (API_URL = 'http://localhost:8888')
-    : (API_URL = 'https://quizify-server.herokuapp.com');
-
-export default API_URL;
