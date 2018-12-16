6f3adbbaea64147bf44016808f6727f4ed456d33
Chris Norman <chris@humm.tech>
2018-12-16 16:58:05 +0800

    remove unneeded server-side code

--- server_old/util/shuffle.js
+++
@@ -1,9 +0,0 @@
-module.exports = a => {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-};
--- server_old/util/urls.js
+++
@@ -1,7 +0,0 @@
-let CLIENT_URL;
-
-process.env.NODE_ENV !== 'production'
-    ? (CLIENT_URL = 'http://localhost:3000')
-    : (CLIENT_URL = 'https://quizify.app'); // digitalocean droplet public ip
-
-module.exports = CLIENT_URL;
